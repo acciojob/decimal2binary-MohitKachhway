3e70bac781a7f9c70ca7a96beba09ace88700e3d
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-07-27 21:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+	let arr=[];
+	
+  while(num!=0){
+	arr.push(num%2)
+	  num=Math.floor(num/2)
+	
+  }
+	let str=""
+	for(let i=arr.length-1;i>=0;++i)
+		{
+			str=str+arr[i];
+		}
+	return str;
 }
 
 window.decimalToBinary = decimalToBinary;
